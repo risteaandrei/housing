ff114fc3ac0a6e35cea55d112838feda80d21ee6
Andrei Ristea <risteaandrei@yahoo.com>
2020-02-13 05:49:51 +0200

    WIP

--- inventory/update_inventory.py
+++ inventory/update_inventory.py
@@ -6,6 +6,127 @@ import json
 import pandas as pd
 import re
 
+import sys
+sys.path.append('tools')
+from housing_common import *
+############################
+## common
+############################
+
+#import boto3
+#import datetime
+#from io import StringIO
+#import json
+#import pandas as pd
+#import os
+#
+#s3 = boto3.client('s3')
+#data_dir = 'data/'
+#output_dir = 'output/'
+#tmp_dir = '/tmp/'
+#first_scraping_day = datetime.date(2020, 1, 6)
+#
+#def key_from_sns_from_s3_put(event):
+#    s3_json = json_loads(event['Records'][0]['Sns']['Message'])
+#    return s3_json['Records'][0]['s3']['object']['key']
+#
+#def string_to_file(data, filename, location=''):
+#    text_file = open(location + filename, 'w')
+#    text_file.write(data)
+#
+#def string_from_file(filename, location=''):
+#    text_file = open(location + filename, 'r')
+#    return text_file.read()
+#
+#def s3_get(key):
+#    object = s3.get_object(Bucket='andrei-housing-prices', Key=key)
+#    data = object['Body'].read().decode('utf-8')
+#    return data
+#
+#def download_from_s3(key, location=''):
+#    string_to_file(s3_get(key), key, location)
+#
+#def s3_put(key, data):
+#    s3.put_object(Bucket='andrei-housing-prices', Key=key, Body=data)
+#
+#def upload_to_s3(key, location=''):
+#    s3_put(key, string_from_file(key, location))
+#
+## JSON
+#
+#def json_from_file(filename, location=''):
+#    return json.load(open(location + filename))
+#
+#def json_to_file(data, filename, location=''):
+#    json.dump(data, open(location + filename, 'w'))
+#
+#def json_from_s3(key):
+#    return json.load(s3_get(key))
+#
+#def json_to_s3(data, key):
+#    s3_put(key, json.dumps(data))
+#
+#def load_json(key, local=False):
+#    if local:
+#        return json_from_file(key, data_dir)
+#    else:
+#        return json_from_s3(key)
+#
+#def save_json(data, key, local=False):
+#    if local:
+#        return json_to_file(data, key, output_dir)
+#    else:
+#        return json_to_s3(data, key)
+#
+## dataframe
+#
+#def df_from_file(filename, location=''):
+#    return pd.read_csv(location + filename, index_col=0, sep='\t')
+#
+#def df_to_file(df, filename, location=''):
+#    df.to_csv(location + filename, sep='\t')
+#
+#def df_from_s3(key):
+#    return pd.read_csv(StringIO(s3_get(key)), index_col=0, sep='\t')
+#
+#def df_to_s3(df, key):
+#    df_to_file(df, key, tmp_dir)
+#    s3_put(key, tmp_dir + key)
+#
+#def load_df(key, local=False):
+#    if local:
+#        return df_from_file(key, data_dir)
+#    else:
+#        return df_from_s3(key)
+#
+#def save_df(data, key, local=False):
+#    if local:
+#        return df_to_file(data, key, output_dir)
+#    else:
+#        return df_to_s3(data, key)
+#
+## date
+#
+#def today():
+#    return datetime.date.today()
+#
+#def date_to_str(date):
+#    return date.strftime('%Y%m%d')
+#
+#def sync_local_with_s3():
+#    day_delta = datetime.timedelta(days=1)
+#    for i in range((today() - first_scraping_day).days + 1):
+#        current_str = date_to_str(first_scraping_day + i * day_delta)
+#        if not os.path.exists(data_dir + current_str):
+#            download_from_s3(current_str, data_dir)
+#
+#    download_from_s3('price_history', data_dir)
+#    download_from_s3('inventory', data_dir)
+
+############################
+## end common
+############################
+
 s3 = boto3.client('s3')
 inventory_key = 'inventory'
 new_key = 'new_today'
@@ -67,8 +188,7 @@ def create_inventory_df(data, key):
     for k in characteristics:
         columns[k] = []
 
-    json_data = json.loads(data)
-    for ap in json_data[key]:
+    for ap in data[key]:
         id.append(ap['id'])
 
         for k, v in characteristics.items():
@@ -80,82 +200,28 @@ def create_inventory_df(data, key):
     df = pd.DataFrame(columns, index=[id])
     return df
 
-def upload_to_s3(file_path, key):
-    data = open(file_path, 'rb')
-    s3.put_object(Bucket='andrei-housing-prices', Key=key, Body=data)
-
 def lambda_handler(event, context):
-    s3_event = event['Records'][0]['Sns']['Message']
-    s3_json = json.loads(s3_event)
-    key = s3_json['Records'][0]['s3']['object']['key']
+    execute(key_from_sns_from_s3_put(event))
 
-    object = s3.get_object(Bucket='andrei-housing-prices', Key=key)
-    data = object['Body'].read().decode('utf-8')
-    today_df = create_inventory_df(data, key)
+def execute(key, local=False):
+    today_json = load_json(key, local)
+    today_df = create_inventory_df(today_json, key)
 
-    object = s3.get_object(Bucket='andrei-housing-prices', Key=inventory_key)
-    data = object['Body'].read().decode('utf-8')
-    data_io = StringIO(data)
-    alltime_df = pd.read_csv(data_io, index_col=0, sep='\t')
-
-    new_df = pd.DataFrame(columns=alltime_df.columns)
-
-    alltime_df['active'] = False
-    for index, row in today_df.iterrows():
-        if index[0] not in alltime_df.index:
-            new_df.loc[index[0]] = row
-
-        alltime_df.loc[index[0]] = row
-        alltime_df.loc[index[0], 'active'] = True
-
-    file_path = '/tmp/' + inventory_key
-    alltime_df.to_csv(file_path, sep='\t')
-    upload_to_s3(file_path, inventory_key)
-
-    if len(new_df.index) > 0:
-        file_path = '/tmp/' + new_key
-        new_df.to_csv(file_path, sep='\t')
-        upload_to_s3(file_path, new_key)
-
-def process_daily_files(start, end, f):
-    for i in range((end - start).days + 1):
-        f((start + datetime.timedelta(days=i)).strftime('%Y%m%d'), inventory_key)
-        #break
-
-def update_df(current_key, inventory_key):
-    print("Updating for " + current_key)
-
-    object = s3.get_object(Bucket='andrei-housing-prices', Key=current_key)
-    current_data = object['Body'].read().decode('utf-8')
-    #current_data = open(current_key, "r")
-    #current_df = create_inventory_df(current_data.read(), current_key)
-
-    try:
-        object = s3.get_object(Bucket='andrei-housing-prices', Key=inventory_key)
-    except:
-        # if this is the first processing ever
-        current_df.to_csv(inventory_key, sep='\t')
-        upload_to_s3(inventory_key)
-        return
-    inventory_data = object['Body'].read().decode('utf-8')
-    inventory_data_io = StringIO(inventory_data)
-    #inventory_data_io = inventory_key
-    inventory_df = pd.read_csv(inventory_data_io, index_col=0, sep='\t')
+    inventory_df = load_df(inventory_key, local)
 
     new_df = pd.DataFrame(columns=inventory_df.columns)
 
     inventory_df['active'] = False
-    for index, row in current_df.iterrows():
+    for index, row in today_df.iterrows():
         if index[0] not in inventory_df.index:
             new_df.loc[index[0]] = row
-            #continue
+
         inventory_df.loc[index[0]] = row
         inventory_df.loc[index[0], 'active'] = True
-        #break
     
-    inventory_df.to_csv(inventory_key, sep='\t')
-    new_df.to_csv(new_key, sep='\t')
+    save_df(inventory_df, inventory_key, local)
+    save_df(new_df, new_key, local)
 
 if __name__ == "__main__":
-    process_daily_files(datetime.datetime(2020, 2, 9), datetime.datetime.now(), update_df)
-    #upload_to_s3('20200208', '20200208')
+    sync_local_with_s3()
+    #execute('tomorrow', True)
