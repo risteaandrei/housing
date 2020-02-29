3b61404e36c9a97526fe85116ca4c066ef46529f
Andrei Ristea <risteaandrei@yahoo.com>
2020-02-29 14:49:26 +0200

    Parse only new appartments pages

--- housing/housing/spiders/appartments_spider.py
+++ housing/housing/spiders/appartments_spider.py
@@ -1,11 +1,17 @@
 import boto3
 import scrapy
+import os
+
 from datetime import date
 from scrapy.crawler import CrawlerProcess
 
+import sys
+sys.path.append('tools')
+from housing_common import *
+
 s3 = boto3.client('s3')
 local_path = 'file:///Users/Andrei/dev/housing/housing/housing/spiders/'
-local_run = 'false'
+local = False
 
 characteristics = [
     "Nr. camere",
@@ -28,21 +34,46 @@ characteristics = [
 class AppartmentsSpider(scrapy.Spider):
     name = "appartments"
 
-    if local_run:
+    inventory_df = load_df('inventory', local)
+
+    if local:
         start_urls = [local_path + 'main_page.html']
     else:
         start_urls = ['https://www.imobiliare.ro/vanzare-apartamente/craiova']
         local_path = ''
+    
+    print("## " + start_urls[0])
 
     def parse(self, response):
-        for appartment in response.css('a.img-block'):
-            appartment_page = local_path + appartment.css('a::attr(href)').get()
-            yield scrapy.Request(appartment_page, callback=self.parse_appartment_page)
-            #break
+        characteristics_dic = {}
 
+        ids = []
+        for id in response.xpath('//div[@itemtype="http://schema.org/Offer"]/@id'):
+            ids.append(id.get()[6:])
+
+        links = []
+        for link in response.css('a.img-block'):
+            links.append(link.css('a::attr(href)').get())
+
+        prices = []
+        for price in response.xpath('//span[@class="pret-mare"]/text()'):
+            prices.append(price.get())
+        
+        app_dict = dict(zip(ids, zip(prices, links)))
+
+        for id in app_dict:
+            if id in self.inventory_df.index:
+                characteristics_dic['id'] = id
+                characteristics_dic['price'] = app_dict[id][0]
+                yield characteristics_dic
+            else:
+                appartment_page = app_dict[id][1]
+                yield scrapy.Request(appartment_page, callback=self.parse_appartment_page)
+        
         next_page = response.xpath('//link[@rel="next"]/@href').get()
         if next_page is not None:
             yield scrapy.Request(next_page, callback=self.parse)
+
 
     def parse_appartment_page(self, response):
         characteristics_dic = {}
@@ -78,27 +109,29 @@ def fix_json(filename, today):
     f.close()
 
 def lambda_handler(event, context):
-    location = '/tmp/result.json'
+    execute()
+
+def execute(local=False):
+    filename = 'result.json'
+    location = '/tmp/'
+    full_path = location + filename
     today = date.today().strftime('%Y%m%d')
+
+    if os.path.exists(full_path):
+        os.remove(full_path)
 
     process = CrawlerProcess({
         'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
         'FEED_FORMAT': 'json',
-        'FEED_URI': location
+        'FEED_URI': full_path
     })
 
     process.crawl(AppartmentsSpider)
     process.start() # the script will block here until the crawling is finished
 
-    fix_json(location, today)
-
-    data = open(location, 'rb')
-    if local_run:
-        print(data)
-    else:
-        s3.put_object(Bucket='andrei-housing-prices', Key=today, Body=data)
+    fix_json(full_path, today)
+    save_json(json.load(open(location + filename)), today, local)
 
 if __name__ == "__main__":
-    local_run = True
-    lambda_handler('', '')
+    execute(True)
 
